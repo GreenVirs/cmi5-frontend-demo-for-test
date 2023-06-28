05c98ef2cc895e428b796e2a1ae982b158a272cd
d.perfilov <work.pda@outlook.com>
2023-06-28 13:00:56 +0400

    add axios

--- src/api/courses.ts
+++ src/api/courses.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { create, find, findOne } from './common';
 import { ICourse, ICourseWithUsers } from '../models/course';
 
@@ -5,11 +6,7 @@ export const getCourses = () => find<ICourse>('courses/all');
 export const getCourse = (id: string) => findOne<ICourseWithUsers>(`courses/${id}`);
 
 export const createCourse = (data: FormData) =>
-  create('courses', data, {
-    headers: new Headers({
-      'Content-Type': 'multipart/form-data',
-    }),
-  });
+  axios.post('http://localhost:5000/api/courses', data);
 
 export const enrollmentCourse = (data: { course_id: string; user_id: string }) =>
-  create('courses/enrollment', data);
+  create('/enrollment', data);
