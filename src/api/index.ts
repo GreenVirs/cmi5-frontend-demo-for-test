e160c60fbe39e85c8bdd5230b6b52a3f8e7efdb8
greenvirs <d.perfilov@magnus-tech.ru>
2023-06-28 11:41:55 +0400

    create course

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,7 @@ import { checkConstructor } from '../utils/check-constructor';
 
 export const BASE_URL = 'http://localhost:3000/api';
 const headers = new Headers({
+  Accept: '*/*',
   'Content-Type': 'application/json;charset=utf-8',
 });
 export const returnError = (err: unknown) => Promise.reject(err);
@@ -14,16 +15,14 @@ const initOptions: RequestInit = {
 const parseHeaders = (hdrs: HeadersInit) =>
   checkConstructor(hdrs, Headers) ? Object.fromEntries(Array.from(hdrs as Headers)) : { ...hdrs };
 
-const mergeHeaders = (headersOne: HeadersInit, headersTwo: HeadersInit): Headers => {
-  console.log('parseHeaders(headersTwo)', parseHeaders(headersTwo));
-  return new Headers({ ...parseHeaders(headersOne), ...parseHeaders(headersTwo) });
-};
+const mergeHeaders = (headersOne: HeadersInit, headersTwo: HeadersInit): Headers =>
+  new Headers({
+    ...parseHeaders(headersOne),
+    ...parseHeaders(headersTwo),
+  });
 const createOptions = (options?: RequestInit) => {
-  console.log('options', options);
-  console.log('checkConstructor(hdrs, Headers)', checkConstructor(options?.headers, Headers));
   const opt: RequestInit =
     typeof options !== 'undefined' ? { ...initOptions, ...options } : { ...initOptions };
-  console.log('headers', opt.headers);
   opt.headers = typeof opt.headers !== 'undefined' ? mergeHeaders(headers, opt.headers) : headers;
   return opt;
 };
